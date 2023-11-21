fbc265d9532fb0603218682e4804b36780625300
LIANG Wenjie <920729404@qq.com>
2023-11-21 14:21:15 +0800

    login和register页面上传

--- app/screens/login/__init__.py
+++ app/screens/login/__init__.py
@@ -5,18 +5,125 @@ from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDRaisedButton
 from kivymd.app import MDApp
 from app.utils.database import db_ref
+from kivymd.uix.dialog import MDDialog
+from kivy.properties import ListProperty, StringProperty, NumericProperty
+from kivymd.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 
 
 class Login(Screen):
 
-
     def login_vertify(self):
-        pass
+        username = self.ids.text_username.text
+        password = self.ids.text_password.ids.text_field.text
 
+        if not username or not password:
+            dialog3 = MDDialog(
+                title='Warning',
+                text='Please enter both username and password.',
+                buttons=[
+                    MDRaisedButton(
+                        text='OK',
+                        on_press=lambda x: dialog3.dismiss(),
+                    )
+                ]
+            )
+            dialog3.open()
+            return
+
+        users_ref = db_ref.child('users')
+        query_result = users_ref.order_by_child('username').get()
+
+        matched = False  #追踪是否找到匹配的用户名和密码
+        query_username = None
+        query_password = None
+
+        if query_result:
+            for result in query_result.values():
+                query_username = result['username']
+                query_password = result['password']
+                if query_username == username and query_password == password:
+                    
+                    matched = True
+                    break
+                
+        if matched:
+            MDApp.get_running_app().switch_screen("navigator")
+            self.show_success_popup
+
+        elif query_username != username or query_password != password:
+            dialog1 = MDDialog(
+                        title = 'Warning',
+                        text = 'Password incorrect!',
+                        buttons = [
+                            MDRaisedButton(
+                                text = 'Return',
+                                on_press = lambda x: dialog1.dismiss(),
+                            )
+                        ]
+                    )
+            dialog1.open()
+
+        else:
+            dialog2 = MDDialog(
+                title='Warning',
+                text='Username does not exist. Please register.',
+                buttons=[
+                    MDRaisedButton(
+                        text='Register',
+                        on_press=lambda x: dialog2.dismiss(),
+                        on_release = MDApp.get_running_app().switch_screen("register"),
+                        
+                    ),
+                    MDRaisedButton(
+                        text='Cancel',
+                        on_press=lambda x: dialog2.dismiss(),
+                        
+                    )
+                ]
+            )
+            dialog2.open()
+             
+
+    def show_success_popup(self):
+            layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+            label = Label(text='Login successful!')
+
+            popup = Popup(title='Success', content=layout, size_hint=(None, None), size=(300, 200))
+            layout.add_widget(label)
+
+            popup.open()
+            Clock.schedule_once(lambda dt: popup.dismiss(), 2)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ClickableTextFieldRound(Screen):
+    text = StringProperty()
+    
+
+
+    
 
 
 
 file_path = os.path.dirname(__file__)
 
 Builder.load_file(os.path.join(file_path,'login.kv'))
+
+
 
--- app/screens/register/__init__.py
+++ app/screens/register/__init__.py
@@ -3,7 +3,6 @@ from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDRaisedButton
-from kivymd.uix.card import MDCard
 from kivymd.app import MDApp
 from app.utils.database import db_ref
 
@@ -11,30 +10,30 @@ from app.utils.database import db_ref
 
 
 class Register(Screen):
-    def register(self):
-        dialog = MDDialog(
-            title = 'Successful registration',
-            text = 'This is your personal information',
-            buttons = [
-                MDRaisedButton(
-                    text = 'Complete Registration',
-                    on_press = lambda x: dialog.dismiss(),
-                    on_release = lambda y: MDApp.get_running_app().switch_screen("login")
-                    ),
-            ]
-        )
-        dialog.open()
+    # def register(self):
+    #     dialog = MDDialog(
+    #         title = 'Successful registration',
+    #         text = 'This is your personal information',
+    #         buttons = [
+    #             MDRaisedButton(
+    #                 text = 'Complete Registration',
+    #                 on_press = lambda x: dialog.dismiss(),
+    #                 on_release = lambda x: MDApp.get_running_app().switch_screen("login")
+    #                 ),
+    #         ]
+    #     )
+    #     dialog.open()
 
     def cancel(self):
         self.ids.text_username.text = ''
-        self.ids.text_password.text = ''
+        self.ids.text_password.ids.text_field.text = ''
         self.ids.text_nickname.text = ''
         self.ids.text_phoneNumber.text = ''
         self.ids.text_address.text = ''
 
     def submit_data(self):
         username = self.ids.text_username.text
-        password = self.ids.text_password.text
+        password = self.ids.text_password.ids.text_field.text
         nickname = self.ids.text_nickname.text
         phoneNumber = self.ids.text_phoneNumber.text
         address = self.ids.text_address.text
@@ -48,13 +47,80 @@ class Register(Screen):
         }
 
         users_ref = db_ref.child('users')
-        users_ref.push(str(data))
+        query1_result = users_ref.order_by_child('username').get()
 
-        self.ids.text_nickname.text = ''
-        self.ids.text_password.text = ''
-        self.ids.text_nickname.text = ''
-        self.ids.text_phoneNumber.text = ''
-        self.ids.text_address.text = ''
+        username_exists = False   #判断username是否已经存在
+
+        if query1_result is not None:
+            for result in query1_result.values():
+                query = result['username']
+                if username == '' or password == '' or nickname == '' or phoneNumber == '' or address == '':
+                    dialog1 = MDDialog(
+                        title = 'Warning',
+                        text = 'Anyone of the text field is empty!',
+                        buttons = [
+                            MDRaisedButton(
+                                text = 'Return',
+                                on_press = lambda x: dialog1.dismiss(),
+                            )
+                        ]
+                    )
+                    dialog1.open()
+                    break
+
+                elif query == username:
+                    username_exists = True
+                    break
+
+        if username_exists:
+
+                    dialog2 = MDDialog(
+                        title = 'Warning',
+                        text = 'Username already exists',
+                        buttons = [
+                            MDRaisedButton(
+                                text = 'Return',
+                                on_press = lambda x: dialog2.dismiss(),
+                            )
+                        ]
+                    )
+                    dialog2.open()
+
+                    # dialog3 = MDDialog(
+                    #     title = 'Warning',
+                    #     text = 'Nickname already exists',
+                    #     buttons = [
+                    #         MDRaisedButton(
+                    #             text = 'Return',
+                    #             on_press = lambda x: dialog3.dismiss(),
+                    #         )
+                    #     ]
+                    # )
+                    # dialog3.open()
+        
+        else:
+            users_ref = db_ref.child('users')
+            users_ref.push(data)
+
+            dialog3 = MDDialog(
+                title = 'Congraduations!',
+                text = 'Registion successful!',
+                buttons = [
+                    MDRaisedButton(
+                        text = 'Finish',
+                        on_press = lambda x: dialog3.dismiss(),
+                        on_release = lambda x:MDApp.get_running_app().switch_screen("login")
+                    )
+                ]
+            )
+            dialog3.open()
+
+            self.ids.text_nickname.text = ''
+            self.ids.text_password.ids.text_field.text = ''
+            self.ids.text_nickname.text = ''
+            self.ids.text_phoneNumber.text = ''
+            self.ids.text_address.text = ''
+
 
 file_path = os.path.dirname(__file__)
 
